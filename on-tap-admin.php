f7e7aaa06ad986c5bd4c95c64409b9529377e95c
Tyler Walker <tyler@lifeblue.com>
2016-07-24 19:31:51 -0500

    Made some more changes

--- on-tap-admin.php
+++ on-tap-admin.php
@@ -5,11 +5,15 @@ global $ontap_dontap_version;
 
 ?>
 
-<h2>On Tap Settings</h2>
+<h1 class="ot-admin__main-heading">On Tap</h1>
 <h2 class="nav-tab-wrapper" id="top-wrap">
     <a class="nav-tab nav-tab-active" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-admin.php">Settings</a>
 </h2>
 
-<section class="ot-admin">
-    <h3 class="ot-admin__heading">Google Maps</h3>
-</section>
+<div class="ot-admin">
+    
+    <section class="ot-section">
+        <h3 class="ot-admin__heading">Google Maps</h3>
+    </section>
+    
+</div>
