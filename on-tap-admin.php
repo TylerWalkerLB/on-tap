cbdc5bee62739f18622cf5b5819224a95442050e
Tyler Walker <tyler@lifeblue.com>
2016-07-24 19:26:25 -0500

    Added navigation tabs and some headings to the admin page

--- on-tap-admin.php
+++ on-tap-admin.php
@@ -5,6 +5,11 @@ global $ontap_dontap_version;
 
 ?>
 
-<div class="on-tap-admin-container">
+<h2>On Tap Settings</h2>
+<h2 class="nav-tab-wrapper" id="top-wrap">
+    <a class="nav-tab nav-tab-active" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-admin.php">Settings</a>
+</h2>
 
-</div>
+<section class="ot-admin">
+    <h3 class="ot-admin__heading">Google Maps</h3>
+</section>
