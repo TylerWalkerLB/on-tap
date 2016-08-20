3ad790fc3b3dc9602fded143c45c43b9f2565f2a
Tyler Walker <tyler@lifeblue.com>
2016-08-19 23:11:07 -0500

    Added new admin pages for viewing all locations "on-tap-locations.php" and for adding a new location "on-tap-add-new.php"

---
+++ on-tap-add-new.php
@@ -0,0 +1,17 @@
+<?php
+
+global $wpdb;
+global $ontap_dontap_version;
+
+?>
+
+<h1 class="ot-admin__main-heading">On Tap</h1>
+<h2 class="nav-tab-wrapper" id="top-wrap">
+    <a class="nav-tab" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-admin.php">Settings</a>
+    <a class="nav-tab nav-tab-active" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-locations.php">Locations</a>
+    <a class="nav-tab" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-add-new.php">Add New</a>
+</h2>
+
+<div class="ot-admin">
+
+</div>
