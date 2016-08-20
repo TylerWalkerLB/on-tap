46812afd86ff3059e7a86d5a92f6308c28094f66
Tyler Walker <tyler@lifeblue.com>
2016-08-19 23:12:08 -0500

    Quick fix to active tab in admin navigation

--- on-tap-locations.php
+++ on-tap-locations.php
@@ -8,8 +8,8 @@ global $ontap_dontap_version;
 <h1 class="ot-admin__main-heading">On Tap</h1>
 <h2 class="nav-tab-wrapper" id="top-wrap">
     <a class="nav-tab" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-admin.php">Settings</a>
-    <a class="nav-tab" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-locations.php">Locations</a>
-    <a class="nav-tab nav-tab-active" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-add-new.php">Add New</a>
+    <a class="nav-tab nav-tab-active" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-locations.php">Locations</a>
+    <a class="nav-tab" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-add-new.php">Add New</a>
 </h2>
 
 <div class="ot-admin">
