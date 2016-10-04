f90e71ae6bd9dda8bfb52317aa5d1a90a7f5d058
Tyler Walker <tyler@lifeblue.com>
2016-10-04 17:07:19 -0500

    Added the list of current locations for the admin panel

---
+++ on-tap-edit.php
@@ -0,0 +1,116 @@
+<?php
+
+global $wpdb;
+global $ontap_dontap_version;
+
+?>
+
+<h1 class="ot-admin__main-heading">On Tap</h1>
+<h2 class="nav-tab-wrapper" id="top-wrap">
+    <a class="nav-tab" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-admin.php">Settings</a>
+    <a class="nav-tab" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-locations.php">Locations</a>
+    <a class="nav-tab" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-add-new.php">Add New</a>
+    <a class="nav-tab nav-tab-active" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-locations.php">Edit Location</a>
+</h2>
+
+<div class="ot-admin">
+    <section class="ot-section">
+        <h3 class="ot-admin__heading">Edit a Location</h3>
+        <h4 class="ot-admin__heading-description">Use this form to edit an existing location that you are on tap at.</h4>
+    </section>
+
+    <section class="ot-section">
+
+        <form class="on-tap-add-edit" data-which="new">
+            <div class="loc">
+                <div class="loc__container">
+                    <label for="loc-title" class="loc__label">Location Name</label>
+                    <input class="loc__input loc__input--text loc-title" name="loc-title" value="">
+                </div>
+
+                <div class="loc__container">
+                    <h2 class="loc__section-title">Address</h2>
+                    <label for="loc-address1" class="loc__label">Street Address 1</label>
+                    <input class="loc__input loc__input--text loc-address1" name="loc-address1" value="">
+                </div>
+
+                <div class="loc__container">
+                    <label for="loc-address2" class="loc__label">Street Address 2</label>
+                    <input class="loc__input loc__input--text loc-address2" name="loc-address2" value="">
+                </div>
+
+                <div class="loc__container loc__container--third">
+                    <label for="loc-city" class="loc__label">City</label>
+                    <input class="loc__input loc__input--text loc-city" name="loc-city" value="">
+                </div>
+
+                <div class="loc__container loc__container--third">
+                    <label for="loc-state" class="loc__label">City</label>
+                    <select name="loc-state" class="loc__input loc__input--select loc-state">
+                        <option value="AL">Alabama</option>
+                        <option value="AK">Alaska</option>
+                        <option value="AZ">Arizona</option>
+                        <option value="AR">Arkansas</option>
+                        <option value="CA">California</option>
+                        <option value="CO">Colorado</option>
+                        <option value="CT">Connecticut</option>
+                        <option value="DE">Delaware</option>
+                        <option value="DC">District of Columbia</option>
+                        <option value="FL">Florida</option>
+                        <option value="GA">Georgia</option>
+                        <option value="HI">Hawaii</option>
+                        <option value="ID">Idaho</option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>
+                        <option value="MA">Massachusetts</option>
+                        <option value="MI">Michigan</option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>
+                        <option value="NH">New Hampshire</option>
+                        <option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX" selected="selected">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin</option>
+                        <option value="WY">Wyoming</option>
+                    </select>
+                </div>
+
+                <div class="loc__container loc__container--third">
+                    <label for="loc-zip" class="loc__label">ZIP</label>
+                    <input class="loc__input loc__input--text loc-zip" name="loc-zip" value="">
+                </div>
+
+                <div class="loc__container loc__container--third">
+                    <button type="submit" class="loc__submit">Add Location</button>
+                </div>
+
+            </div>
+        </form>
+    </section>
+</div>
--- on-tap-locations.php
+++ on-tap-locations.php
@@ -1,7 +1,19 @@
 <?php
 
+require_once(ABSPATH.'wp-config.php');
+require_once(ABSPATH.'wp-load.php');
 global $wpdb;
 global $ontap_dontap_version;
+
+$table_name = $wpdb->prefix.'on_tap_locations';
+
+$locations = $wpdb->get_results(
+    "
+        SELECT *
+        FROM $table_name
+        WHERE deleted = 1
+        "
+);
 
 ?>
 
@@ -13,5 +25,45 @@ global $ontap_dontap_version;
 </h2>
 
 <div class="ot-admin">
+    <section class="ot-section">
+        <h3 class="ot-admin__heading">All Locations</h3>
 
+        <table class="ot-table">
+            <tr class="ot-table__row ot-table__row--head">
+                <th class="ot-table__head">Name</th>
+                <th class="ot-table__head">Address</th>
+                <th class="ot-table__head">Status</th>
+                <th class="ot-table__head">Options</th>
+            </tr>
+
+            <?php foreach($locations as $loc): ?>
+                <tr class="ot-table__row">
+                    <td class="ot-table__cell">
+                        <a class="ot-table__link" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-edit.php&loc=<?php $loc->id; ?>">
+                            <?php echo $loc->location_name; ?>
+                        </a>
+                    </td>
+                    <td class="ot-table__cell">
+                        <?php
+                        echo $loc->location_address1;
+                        echo !empty($loc->location_address2) ? ' '.$loc->location_address2.', ' : ', ';
+                        echo $loc->location_city . ', ' . $loc->location_state . ' ' . $loc->location_zip;
+
+                        ?>
+                    </td>
+                    <td class="ot-table__cell">
+                        <span class="ot-table__status <?php echo ($loc->on_tap) ? 'ot-table__status--enabled' : 'ot-table__status--disabled'; ?>">
+                            <?php echo ($loc->on_tap) ? 'Enabled' : 'Disabled'; ?>
+                        </span>
+                    </td>
+                    <td class="ot-table__cell">
+                        <a class="ot-table__button ot-table__button--edit" href="<?php echo admin_url(); ?>admin.php?page=on-tap%2Fon-tap-edit.php&loc=<?php $loc->id; ?>">
+                            Edit
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+
+        </table>
+    </section>
 </div>
